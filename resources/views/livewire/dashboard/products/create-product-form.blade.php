01fb5e114a57a3b123c21129b7a5fc199b9819c5
Ahmad Alsakhen <73029268+Al-sakhen@users.noreply.github.com>
2025-03-17 00:36:59 +0300

    Ahmad Alsakhen 17-3-2025 12:30AM

--- resources/views/livewire/dashboard/products/create-product-form.blade.php
+++ resources/views/livewire/dashboard/products/create-product-form.blade.php
@@ -260,7 +260,7 @@
             <div class="card">
                 <div class="card-header">
                     <h6>
-                        Product Images <span x-text='isUploading'></span>
+                        Product Images 
                     </h6>
                 </div>
                 <div class="card-body">
