302790ddffc17bd73a5909dd76adeca60b60a810
Ahmad Alsakhen <73029268+Al-sakhen@users.noreply.github.com>
2025-04-20 22:07:50 +0300

    Ahmad Alsakhen 20-4-2025 8:45PM

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -5,42 +5,44 @@
 @section('content')
     {!! $page->content !!}
     {{-- Categories Section Grid 6 columns --}}
-    <div class="home1-category-section mb-120">
-        <div class="container">
-            <div class="flex-wrap gap-3 row mb-3 align-items-center justify-content-between wow animate fadeInDown"
-                data-wow-delay="200ms" data-wow-duration="1500ms">
-                <div class="col-12">
-                    <div class="section-title">
-                        <h3>Categories</h3>
-                        <p>
-                            Explore our diverse range of categories, each offering a unique selection of high-quality
-                            home décor products. Find the perfect items to suit your style and needs.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                @forelse ($categories as $category)
-                    <div class="col-lg-2 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="200ms"
-                        data-wow-duration="1500ms">
-                        <a class="card text-center p-2 category-card"
-                            href="{{ route('frontend.shop', ['category' => $category->slug]) }}"
-                            style="background-image: url({{ asset($category->image) }}) ; background-size: cover;">
-                            <div class="card-body">
-                                <h5 class="m-0">
-                                    {{ $category->name }}
-                                </h5>
-                            </div>
-                        </a>
-                    </div>
-                @empty
-                @endforelse
-            </div>
-        </div>
-    </div>
+
 
 
     @if (showCategoryAndShopPages() || auth('dealer')->check() || auth('representative')->check() || auth('web')->check())
+        <div class="home1-category-section mb-120">
+            <div class="container">
+                <div class="flex-wrap gap-3 row mb-3 align-items-center justify-content-between wow animate fadeInDown"
+                    data-wow-delay="200ms" data-wow-duration="1500ms">
+                    <div class="col-12">
+                        <div class="section-title">
+                            <h3>Categories</h3>
+                            <p>
+                                Explore our diverse range of categories, each offering a unique selection of high-quality
+                                home décor products. Find the perfect items to suit your style and needs.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    @forelse ($categories as $category)
+                        <div class="col-lg-2 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="200ms"
+                            data-wow-duration="1500ms">
+                            <a class="card text-center p-2 category-card"
+                                href="{{ route('frontend.shop', ['category' => $category->slug]) }}"
+                                style="background-image: url({{ asset($category->image) }}) ; background-size: cover;">
+                                <div class="card-body">
+                                    <h5 class="m-0">
+                                        {{ $category->name }}
+                                    </h5>
+                                </div>
+                            </a>
+                        </div>
+                    @empty
+                    @endforelse
+                </div>
+            </div>
+        </div>
+
         <!-- Home1 General Art Section Start -->
         <div class="home1-general-art-slider-section mb-120">
             <div class="container">
