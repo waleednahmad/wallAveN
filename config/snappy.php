77bb4c36ea76d9f753dca1b25ef740bdadabfc45
Ahmad Alsakhen <73029268+Al-sakhen@users.noreply.github.com>
2025-05-15 01:32:14 +0300

    Ahmad Alsakhen 15-5-2025 1:25 AM

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary'  => "../storage/wkhtmltopdf/bin/wkhtmltopdf.exe",
+        'binary'  => "../storage/wkhtmltopdf/bin/wkhtmltopdf",
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
@@ -45,7 +45,7 @@ return [
 
     'image' => [
         'enabled' => true,
-        'binary'  => "../storage/wkhtmltopdf/bin/wkhtmltoimage.exe",
+        'binary'  => "../storage/wkhtmltopdf/bin/wkhtmltoimage",
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
