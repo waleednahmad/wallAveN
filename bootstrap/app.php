9c3fa75e237c2eca767d1ad9e663bdef2c8f259d
Ahmad Alsakhen <73029268+Al-sakhen@users.noreply.github.com>
2026-03-24 01:23:22 +0300

    Add middleware to trust proxies in application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
